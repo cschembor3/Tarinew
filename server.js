427e82ea5b589c7486ab8f978516b0ba0b0ca1b2
Connor Schembor <cschembor96@gmail.com>
2019-07-05 13:42:58 -0400

    Add name retrieval from db

--- server.js
+++ server.js
@@ -13,29 +13,53 @@ const port = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+/*
+ * Inserts the character information into the database
+ */
 app.post('/', async function (req, res) {
-    let characterName = req.body.characterName;
-    let characterRace = req.body.characterRace;
-    let characterClass = req.body.characterClass;
-    let characterLevel = req.body.characterLevel;
-    try {
-      const client = await pool.connect();
-      const dbReq = await client.query('INSERT INTO character_info_table (id, name, race, class, level) VALUES ($1, $2, $3, $4, $5)',
-        [2, characterName, characterRace, characterClass, characterLevel], (error, response) => {
-          if (error) {
-            throw error;
-          }
-        });
-      const result = await client.query('SELECT * FROM character_info_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.json({
-        response: results
+  let characterName = req.body.characterName;
+  let characterRace = req.body.characterRace;
+  let characterClass = req.body.characterClass;
+  let characterLevel = req.body.characterLevel;
+  try {
+    const client = await pool.connect();
+    const dbReq = await client.query('INSERT INTO character_info_table (id, name, race, class, level) VALUES ($1, $2, $3, $4, $5)',
+      [2, characterName, characterRace, characterClass, characterLevel], (error, response) => {
+        if (error) {
+          throw error;
+        }
       });
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
+    const result = await client.query('SELECT * FROM character_info_table');
+    const results = { 'results': (result) ? result.rows : null};
+    res.json({
+      response: results
+    });
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
+
+/*
+ * Gets the character information for the given id
+ */
+app.get('/players/:playerId', async function(req, res) {
+  try {
+    const client = await pool.connect();
+    const playerId = req.param('playerId');
+    const characterInfoResp = await client.query(
+      'SELECT * FROM character_info_table WHERE name == $1', [playerId]);
+    const characterInfoResult = { 'results'
+      : (characterInfoResp)
+      ? characterInfoResp.rows : null};
+    res.json({
+      response: characterInfoResult
+    })
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
 });
 
 http.createServer(app).listen(port);
