322d60b3edfb91a579827676e6135d1852154179
Connor Schembor <cschembor96@gmail.com>
2019-07-07 16:47:56 -0400

    Fix item db query

--- server.js
+++ server.js
@@ -50,6 +50,7 @@ app.post('/', async function (req, res, next) {
  */
 app.post('/players/:playerId/items', async function(req, res, next) {
   const client = await pool.connect();
+  const data = {id: req.params.playerId, name: req.body.itemName, description: req.body.itemDescription };
   const playerId = req.params.playerId;
   const itemName = req.body.itemName;
   const itemDescription = req.body.itemDescription;
@@ -62,7 +63,7 @@ app.post('/players/:playerId/items', async function(req, res, next) {
     await pool.query(
       'INSERT INTO items ' +
       '(itemname, itemdescription, charactername) VALUES ($1, $2, $3);' +
-      [itemName, itemDescription, playerId],
+      [data.name, data.description, data.id],
       (error, response) => {
         if (error) {
           res.send('Error: ' + error
