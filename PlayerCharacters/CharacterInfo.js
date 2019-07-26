bfbd7a62ec4d84f80b7f0f622c98d2f306d7cf91
Connor Schembor <cschembor96@gmail.com>
2019-07-25 22:57:28 -0400

    Test

--- PlayerCharacters/CharacterInfo.js
+++ PlayerCharacters/CharacterInfo.js
@@ -41,7 +41,7 @@ function getCharacterInfo(characterName) {
  * Add a description for the specified character to the database
  */
 function addDescription(characterName, description) {
-    const characterDescription = $('.btn.addDesc').val();
+    const characterDescription = $(description).val();
     $.post(
         'https://boiling-sea-30343.herokuapp.com/players/' + characterName + '/description',
         {
