0aa0c964d479a211a2e5a91db895beb532879412
Connor Schembor <cschembor96@gmail.com>
2019-07-28 17:31:39 -0400

    First release

--- Homeage.js
+++ Homeage.js
@@ -2,6 +2,5 @@ import { get } from "https";
 
 function getPlayerCharacters() {
     $.ajax({url: "https://boiling-sea-30343.herokuapp.com/", dataType: "json", success: function(data) {
-        
     }});
 }
